cf0307399c6a6d5241d4c352c5d64cd7b49b6fa0
justinmatsnev <jmatsnev@gmail.com>
2019-11-18 20:25:56 -0500

    first commit

---
+++ Coffee Club/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee Club
+//
+//  Created by Justin Matsnev on 11/18/19.
+//  Copyright © 2019 Justin Matsnev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
